b528f87550adecc392797feb7419d6a21130f231
Legendnumberone <ds.fouluk@gmail.com>
2021-12-12 17:30:34 +0300

    V.0.0.1

---
+++ precache-manifest.5dfcb5ac8aee7ba45cff75c1490f60eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aecc78c6d13d012f6728",
+    "url": "./static/css/main.09ed430d.chunk.css"
+  },
+  {
+    "revision": "aecc78c6d13d012f6728",
+    "url": "./static/js/main.08f07147.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "0ba674501d32b03a78ab",
+    "url": "./static/css/2.1cf3003d.chunk.css"
+  },
+  {
+    "revision": "0ba674501d32b03a78ab",
+    "url": "./static/js/2.a5b6c3d8.chunk.js"
+  },
+  {
+    "revision": "135a74e1c6e6fd3c1a29c3587a387a10",
+    "url": "./static/media/click.135a74e1.png"
+  },
+  {
+    "revision": "961cd869336473f18222ecc4b417668c",
+    "url": "./index.html"
+  }
+];
